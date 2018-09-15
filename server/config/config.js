aa231b373bfecbcdd769a406d370c10f8d52eff3
Yohanni Arnedo <jvo_991@hotmail.com>
2018-09-15 14:03:42 -0500

    Fix package.json

--- server/config/config.js
+++ server/config/config.js
@@ -4,6 +4,6 @@ if (env === 'development') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://admin:admin1055912@ds143932.mlab.com:43932/qrvey-time';
 } else if (env === 'production') {
-  process.env.PORT = 3000;
+  
   process.env.MONGODB_URI = 'mongodb://admin:admin1055912@ds143932.mlab.com:43932/qrvey-time';
 }
